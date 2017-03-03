d7cafc7fcbe99c6cd0b2cb5659ffa1ae7b90a290
gonechar <gonechar@gmail.com>
2017-03-04 02:05:14 +0600

    Added specified resources links in the way: http://ip:port/{applicationContextRoot}/models/model_name/object_name/property_name/script_name

--- src/main/java/bat15/restful/requests/ThingPropertyResource.java
+++ src/main/java/bat15/restful/requests/ThingPropertyResource.java
@@ -6,8 +6,14 @@
 package bat15.restful.requests;
 
 import bat15.restful.process.ThingPropertyProcessor;
+import java.io.IOException;
 import javax.ejb.EJB;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -16,15 +22,17 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
 /**
  *
  * @author Павел
  */
-@Path("models")
+@Path("/")
 public class ThingPropertyResource {
     
     @Context
@@ -49,33 +57,64 @@ public class ThingPropertyResource {
      * @param totime
      * @return an instance of ru.beeline.hadoop.webtechccboonline.ws.WsObject
      */
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String getTestRequest(@QueryParam("json") String jsonType) {
+////        return new WsObject("examp.ru","/usrl/fhsj", (new Date()).getTime());
+//        
+//
+//        System.out.println("json: "+jsonType);
+//        
+//        System.out.println("test");
+//        
+//        int type = 0;
+//        
+//        if(jsonType!=null) 
+//            if(jsonType.equals("short")) type = 1;
+//            else if(jsonType.equals("full")) type = 0;
+//        
+//        return proc.getPropertyValue(type);
+//    }
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getTestRequest(@QueryParam("json") String jsonType) {
+    public String getNoModel(@QueryParam("json") String jsonType) {
 //        return new WsObject("examp.ru","/usrl/fhsj", (new Date()).getTime());
-        
-
-        System.out.println("json: "+jsonType);
-        
-        System.out.println("test");
-        
-        int type = 0;
-        
-        if(jsonType!=null) 
-            if(jsonType.equals("short")) type = 1;
-            else if(jsonType.equals("full")) type = 0;
-        
-        return proc.getPropertyValue(type);
+                
+        return proc.getNoModel();
     }
     
+    
+    
+//    @GET
+//    @Path("{path:}")
+//    @Produces("text/html")
+//    public Response redirectToIndex(@Context HttpServletRequest request, @Context HttpServletResponse response)
+//            throws IOException {
+//        String myIndexPage = "/index.html";
+//        String contextPath = request.getContextPath();
+//        response.sendRedirect(contextPath + myIndexPage);
+//        return Response.status(Status.ACCEPTED).build();
+//    }
+//        
     
     @GET
     @Path("{path:.*}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getModelRequest(@PathParam("path") String modelQuery) {
+    public String getModelRequest(@Context UriInfo uriInfo,@Context HttpHeaders headers, @PathParam("path") String modelQuery) {
     
+        //ui.getBaseUri();
         return proc.getModel(modelQuery);
     }
+
+    
+//    @GET
+//    @Path("{path:.*}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String getModelRequest(@PathParam("path") String modelQuery) {
+//    
+//        return proc.getModel(modelQuery);
+//    }
   
   
 //    
@@ -101,14 +140,24 @@ public class ThingPropertyResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String data) {
         proc.putPropertyValue(data);
-    }      
+    } 
     
+    
+
+    
+//    //bean injection as parameter
+//    @POST
+//    public void post(@BeanParam MyBeanParam beanParam, String entity) {
+//        final String pathParam = beanParam.getPathParam(); // contains injected path parameter "p"
+//    }
+
+
     @POST
     @Path("/post")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createDataInJSON(String data) { 
+    public Response createDataInJSON(@DefaultValue("green") @FormParam("name") String name) { 
 
-        String result = "Data post: "+data;
+        String result = "Data post: "+name;
 
         return Response.status(201).entity(result).build(); 
     }    
